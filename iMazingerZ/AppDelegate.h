8ffb97d826e0acddb578b77030e63da9b1e986a3
Akixe <akixe.otegi@gmail.com>
2016-03-06 16:00:06 +0100

    Initial Commit

---
+++ iMazingerZ/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iMazingerZ
+//
+//  Created by Akixe on 6/3/16.
+//  Copyright © 2016 AOA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
